0ded944b670138237239d0c12c519ac03995a1ab
ahmadhss366 <ahmadhss366@gmail.com>
2024-02-12 10:38:40 +0200

    solve small issue regarding sendVMDataTxn

--- packages/core/src/wallet/wallet.ts
+++ packages/core/src/wallet/wallet.ts
@@ -235,8 +235,8 @@ export default class PWRWallet {
 
         const txnDataBytes = generateDataTxnBytes(
             id,
-            _nonce,
             _chainId,
+            _nonce,
             _vmId,
             data
         );
