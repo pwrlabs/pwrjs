0c3a543530f918d0821b7f614c12bfe86ea7e5ab
loque18 <madridx2010@gmail.com>
2024-05-09 02:27:23 -0500

    feat: added export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,7 @@
 import PWRWallet from './wallet/wallet';
 import PWRJS from './protocol/pwrjs';
 
+import TransactionDecoder from './protocol/transaction-decoder';
+
 // todo: split into separate files
-export { PWRWallet, PWRJS };
+export { PWRWallet, PWRJS, TransactionDecoder };
