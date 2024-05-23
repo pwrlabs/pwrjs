fcbc76abdb33adfb1c24ae100a2cd50f3fca1c7c
loque18 <madridx2010@gmail.com>
2024-05-22 19:20:45 -0500

    feat: export txn builder

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ import PWRWallet from './wallet/wallet';
 import PWRJS from './protocol/pwrjs';
 
 import TransactionDecoder from './protocol/transaction-decoder';
+import TransactionBuilder from './protocol/transaction-builder';
 
 // todo: split into separate files
-export { PWRWallet, PWRJS, TransactionDecoder };
+export { PWRWallet, PWRJS, TransactionDecoder, TransactionBuilder };
