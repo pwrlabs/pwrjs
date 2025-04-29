c93aa8f97d4aa5df57711623e83775efb9a4f600
loque18 <madridx2017@gmail.com>
2025-04-29 08:20:28 -0500

    feat: added services to exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,13 +1,18 @@
-// core
-import Falcon512Wallet from './wallet/falcon-512-wallet';
-import PWRJS from './protocol/pwrjs';
+// // core
+// import Falcon512Wallet from './wallet/falcon-512-wallet';
+// import PWRJS from './protocol/pwrjs';
 
-// export services
-import DeterministicSecureRandom from './services/secure-random.service';
+// // export services
+// import DeterministicSecureRandom from './services/secure-random.service';
 
-const services = {
-    DeterministicSecureRandom,
-};
+// const services = {
+//     DeterministicSecureRandom,
+// };
 
-// todo: split into separate files add package.json for each module
-export { Falcon512Wallet, PWRJS };
+// // todo: split into separate files add package.json for each module
+// export { Falcon512Wallet, PWRJS };
+
+export * from './exports/types';
+export * from './exports/utils';
+export * from './exports/main';
+export * from './exports/services';
