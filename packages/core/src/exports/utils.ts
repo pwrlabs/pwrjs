5a0fdb78b3f2f9dc311a59d282f75aec2b55bce3
loque18 <madridx2017@gmail.com>
2025-05-08 06:42:49 -0500

    note: built package

--- packages/core/src/exports/utils.ts
+++ packages/core/src/exports/utils.ts
@@ -1,5 +1,6 @@
 import FalconTransactionBuilder from '../protocol/falcon-transaction-builder';
 import { hexToBytes, bytesToHex } from 'src/utils';
 
+// added
 export { FalconTransactionBuilder as TransactionBuilder };
 export { hexToBytes, bytesToHex };
