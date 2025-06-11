e17a5645d366bf25cf342104e64fd52825878db0
Khairallah AL-Awady <khairallahalawadi@gmail.com>
2025-06-11 18:18:16 +0300

    update

--- packages/browser-wallet/src/index.ts
+++ packages/browser-wallet/src/index.ts
@@ -1,11 +1,16 @@
 import BrowserWallet from './wallet/browserWallet';
 
-import { TransactionTypes } from './types/transaction.types';
-
 import { connect, disconnect, isInstalled, getConnection, getEvent } from './wallet/connection';
-
-export { BrowserWallet, connect, disconnect, isInstalled, getConnection, getEvent };
 
 if (typeof window === 'undefined') {
     throw new Error('BrowserWallet can only be used in a browser environment.');
+}
+
+export {
+    BrowserWallet,
+    connect,
+    disconnect,
+    isInstalled,
+    getConnection,
+    getEvent,
 }
