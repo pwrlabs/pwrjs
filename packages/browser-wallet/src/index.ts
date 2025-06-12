02f7221bcf9d6143da3594ca199665e5be0f1069
Khairallah AL-Awady <khairallahalawadi@gmail.com>
2025-06-12 15:18:12 +0300

    chore: 🔧 Update browser package

--- packages/browser-wallet/src/index.ts
+++ packages/browser-wallet/src/index.ts
@@ -2,10 +2,6 @@ import BrowserWallet from './wallet/browserWallet';
 
 import { connect, disconnect, isInstalled, getConnection, getEvent } from './wallet/connection';
 
-if (typeof window === 'undefined') {
-    throw new Error('BrowserWallet can only be used in a browser environment.');
-}
-
 export {
     BrowserWallet,
     connect,
